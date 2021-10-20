0fee45135887d503053327b1c26af3a5990a1d43
mfma <cxksmyt@126.com>
2021-10-20 17:28:25 +0800

    add ribbon  support
    add ribbon  support

---
+++ ribbon-learning/eureka-master-server/src/main/java/com/mfma/eurekamasterserver/config/EurekaStateChangeListener.java
@@ -0,0 +1,33 @@
+package com.mfma.eurekamasterserver.config;
+
+import com.netflix.appinfo.InstanceInfo;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.cloud.netflix.eureka.server.event.*;
+import org.springframework.context.event.EventListener;
+import org.springframework.stereotype.Component;
+
+@Slf4j
+@Component
+public class EurekaStateChangeListener {
+	@EventListener
+	public void listen(EurekaInstanceCanceledEvent event) {
+		log.error(event.getServerId() + "\t" + event.getAppName() + " 服务下线 ");
+	}
+	@EventListener
+	public void listen(EurekaInstanceRegisteredEvent event) {
+		InstanceInfo instanceInfo = event.getInstanceInfo();
+		log.info(instanceInfo.getAppName() + " 进行注册 ");
+	}
+	@EventListener
+	public void listen(EurekaInstanceRenewedEvent event) {
+		log.info(event.getServerId() + "\t" + event.getAppName() + " 服务进行续约 ");
+	}
+	@EventListener
+	public void listen(EurekaRegistryAvailableEvent event) {
+		log.info(" 注册中心启动 ");
+	}
+	@EventListener
+	public void listen(EurekaServerStartedEvent event) {
+		log.info("Eureka Server启动 ");
+	}
+}
---
+++ ribbon-learning/service-consumer/src/main/java/com/mfma/serviceconsumer/ServiceConsumerApplication.java
@@ -0,0 +1,16 @@
+package com.mfma.serviceconsumer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+
+@SpringBootApplication
+@EnableEurekaClient
+public class ServiceConsumerApplication {
+	
+	public static void main(String[] args) {
+		SpringApplication.run(ServiceConsumerApplication.class, args);
+	}
+	
+}
