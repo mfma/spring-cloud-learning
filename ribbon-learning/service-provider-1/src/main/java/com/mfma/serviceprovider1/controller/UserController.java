6dcbefe27f6b7cc85cf77f2fe82677530671d2cf
mfma <cxksmyt@126.com>
2021-10-20 20:59:02 +0800

    Test the effect of load balancing
    Test the effect of load balancing

---
+++ ribbon-learning/service-provider-1/src/main/java/com/mfma/serviceprovider1/controller/UserController.java
@@ -0,0 +1,17 @@
+package com.mfma.serviceprovider1.controller;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+
+@Slf4j
+@RestController
+public class UserController {
+	
+	@GetMapping("/user/hello")
+	public String hello() {
+		log.debug("=========有人请求我==========");
+		return "hello,this is service-provider-1";
+	}
+}
