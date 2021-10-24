956d73fc2172d6a349babf94df24271904ab3afe
mfma <cxksmyt@126.com>
2021-10-24 23:00:24 +0800

    modify eureka to prepare to support gateway
    modify eureka to prepare to support gateway

---
+++ gateway-learning/eureka-slave-server-2/src/test/java/com/mfma/eurekaslaveserver2/EurekaSlaveServer2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mfma.eurekaslaveserver2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EurekaSlaveServer2ApplicationTests {
+	
+	@Test
+	void contextLoads() {
+	}
+	
+}
